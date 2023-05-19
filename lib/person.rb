cacc0efa31427bd749544a81478164952c88fcb8
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-19 11:03:35 +0300

    person-class

--- lib/person.rb
+++ lib/person.rb
@@ -4,58 +4,7 @@ class Person
   attr_accessor :bank_account
   attr_writer :happiness
 
-  def initialize(name)
-    @name = name
-    @bank_account = 25
-    @happiness = 8
-    @hygiene = 8
-  end
+end    
 
 
-  def happiness
-    @happiness
-  end
-
-
-  def happiness=(value)
-    if value > 10
-      @happiness = 10
-    elsif value < 0
-      @happiness = 0
-    else
-      @happiness = value
-    end
-  end
-
-
-  def hygiene
-    @hygiene
-  end
-
-  def hygiene=(value)
-    if value > 10
-      @hygiene = 10
-    elsif value < 0
-      @hygiene = 0
-    else
-      @hygiene = value
-    end
-  end
-
-
-  def clean?
-    hygiene > 7
-  end
-
-
-  def happy?
-    happiness > 7
-  end
-
-
-  def get_paid(amount)
-    self.bank_account += amount
-    "all about the benjamins"
-  end
-end
-
+  
