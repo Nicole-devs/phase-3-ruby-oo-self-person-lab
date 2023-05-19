c08c099497634ed927c970ff8b1b63db9955a5c1
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-19 11:24:54 +0300

    work_out

--- lib/person.rb
+++ lib/person.rb
@@ -64,6 +64,13 @@ class Person
     self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
+
+
+  def work_out
+    self.hygiene -=3
+    self.happiness +=2
+    "♪ another one bites the dust ♫"
+  end
 end   
 
 
