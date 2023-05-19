04141bee41aec3f6430870bb9a4224d40fd7be16
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-19 11:04:46 +0300

    initialize

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,12 @@ class Person
   attr_accessor :bank_account
   attr_writer :happiness
 
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
 end    
 
 
